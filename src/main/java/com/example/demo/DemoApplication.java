f40d3842ec062ff2f5cb24539d51a5c04c2b9db4
tswlivia <149760707+tswlivia@users.noreply.github.com>
2023-11-24 05:37:46 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController 
+@SpringBootApplication 
 public class DemoApplication {
-
+	@GetMapping("/") 
+	public String hello() {
+		return "<html><body><h1>Hello DevSecOps</body></h1></html>";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
+	} 
 }
